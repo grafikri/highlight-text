5e21cfdfa1649c0fe2707819d1177f7aa08ff0bd
Serhan Coşkun <grafikri@gmail.com>
2020-02-18 23:23:35 +0300

    some tests added

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,27 @@
 // Birazdan buralar karışacak
 const Highlight = require("../dist")
 
-test("Basic", () => {
+test("Basic search", () => {
   const highlight = new Highlight()
   const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
 
   expect(result).toEqual("Merhaba <span class='highlight-text'>Dünya</span>")
+})
+
+test("Tag should be same as given", () => {
+  const highlight = new Highlight({
+    tag: "i"
+  })
+  const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
+
+  expect(result).toEqual("Merhaba <i class='highlight-text'>Dünya</i>")
+})
+
+test("Quote should be double", () => {
+  const highlight = new Highlight({
+    quotation: "double"
+  })
+  const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
+
+  expect(result).toEqual('Merhaba <span class="highlight-text">Dünya</span>')
 })
