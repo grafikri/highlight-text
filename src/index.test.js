e79c855441da77793582451d6d70ad448dea4690
Serhan Coşkun <grafikri@gmail.com>
2020-02-18 23:15:47 +0300

    first test added

--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,9 @@
 // Birazdan buralar karışacak
+const Highlight = require("../dist")
+
+test("Basic", () => {
+  const highlight = new Highlight()
+  const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
+
+  expect(result).toEqual("Merhaba <span class='highlight-text'>Dünya</span>")
+})
