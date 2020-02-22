adccd4c8814977d758313ebd51dbf91b3517fd7b
Serhan Coşkun <grafikri@gmail.com>
2020-02-22 23:32:51 +0300

    tests written

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,28 +1,133 @@
 import HighlightText, { QuotationType } from "./index"
 
-test("Basic typescript search", () => {
-  const text = new HighlightText()
-  const replaceWord = text.replaceWord("Merhaba Dünya", "Merhaba")
+describe("Turkish", () => {
+  test("case sensitivity", () => {
+    const text = new HighlightText()
 
-  expect(replaceWord).toEqual(
-    "<span class='highlight-text'>Merhaba</span> Dünya"
-  )
+    // Text letter is uppercase
+    // Search letter is upercase
+    expect(text.replaceWord("İzmir Bursa İstanbul", "İzmir İstanbul")).toEqual(
+      "<span class='highlight-text'>İzmir</span> Bursa <span class='highlight-text'>İstanbul</span>"
+    )
+
+    // Text letter is uppercase
+    // Search letter is lowercase
+    expect(text.replaceWord("İzmir Bursa İstanbul", "izmir istanbul")).toEqual(
+      "<span class='highlight-text'>İzmir</span> Bursa <span class='highlight-text'>İstanbul</span>"
+    )
+
+    // Text letter is lowercase
+    // Search letter is uppercase
+    expect(text.replaceWord("izmir Bursa istanbul", "İzmir İstanbul")).toEqual(
+      "<span class='highlight-text'>izmir</span> Bursa <span class='highlight-text'>istanbul</span>"
+    )
+
+    // Text letter is lowercase
+    // Search letter is lowercase
+    expect(text.replaceWord("izmir Bursa istanbul", "izmir istanbul")).toEqual(
+      "<span class='highlight-text'>izmir</span> Bursa <span class='highlight-text'>istanbul</span>"
+    )
+  })
 })
 
-test("Tag should be same as given", () => {
-  const highlight = new HighlightText({
-    tag: "i"
-  })
-  const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
+describe("English", () => {
+  test("case sensitivity", () => {
+    const text = new HighlightText({
+      language: "en"
+    })
 
-  expect(result).toEqual("Merhaba <i class='highlight-text'>Dünya</i>")
+    // Text letter is uppercase
+    // Search letter is upercase
+    expect(text.replaceWord("Londra Stockholm ParIs", "Londra ParIs")).toEqual(
+      "<span class='highlight-text'>Londra</span> Stockholm <span class='highlight-text'>ParIs</span>"
+    )
+
+    // Text letter is uppercase
+    // Search letter is lowercase
+    expect(text.replaceWord("Londra Stockholm ParIs", "Londra Paris")).toEqual(
+      "<span class='highlight-text'>Londra</span> Stockholm <span class='highlight-text'>ParIs</span>"
+    )
+
+    // Text letter is lowercase
+    // Search letter is uppercase
+    expect(text.replaceWord("Londra Stockholm Paris", "Londra ParIs")).toEqual(
+      "<span class='highlight-text'>Londra</span> Stockholm <span class='highlight-text'>Paris</span>"
+    )
+
+    // Text letter is lowercase
+    // Search letter is lowercase
+    expect(text.replaceWord("Londra Stockholm Paris", "Londra Paris")).toEqual(
+      "<span class='highlight-text'>Londra</span> Stockholm <span class='highlight-text'>Paris</span>"
+    )
+  })
 })
 
-test("Quote should be double", () => {
-  const highlight = new HighlightText({
-    quotation: QuotationType.Single
-  })
-  const result = highlight.replaceWord("Merhaba Dünya", "Dünya")
+describe("Tag", () => {
+  test("Default Tag should be span", () => {
+    const highlight = new HighlightText()
 
-  expect(result).toEqual("Merhaba <span class='highlight-text'>Dünya</span>")
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<span class='highlight-text'>izmir</span> bursa istanbul"
+    )
+  })
+
+  test("Tag should be same as given", () => {
+    const highlight = new HighlightText({
+      tag: "i"
+    })
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<i class='highlight-text'>izmir</i> bursa istanbul"
+    )
+  })
+})
+
+describe("Quote", () => {
+  test("Default quote should be single", () => {
+    const highlight = new HighlightText()
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<span class='highlight-text'>izmir</span> bursa istanbul"
+    )
+  })
+
+  test("Quote should be single", () => {
+    const highlight = new HighlightText({
+      quotation: QuotationType.Single
+    })
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<span class='highlight-text'>izmir</span> bursa istanbul"
+    )
+  })
+
+  test("Quote should be double", () => {
+    const highlight = new HighlightText({
+      quotation: QuotationType.Double
+    })
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      '<span class="highlight-text">izmir</span> bursa istanbul'
+    )
+  })
+})
+
+describe("ClassName", () => {
+  test("Default className should be highlight-text", () => {
+    const highlight = new HighlightText()
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<span class='highlight-text'>izmir</span> bursa istanbul"
+    )
+  })
+
+  test("Default className should be as given", () => {
+    const highlight = new HighlightText({
+      className: "front-end-developer"
+    })
+
+    expect(highlight.replaceWord("izmir bursa istanbul", "izmir")).toEqual(
+      "<span class='front-end-developer'>izmir</span> bursa istanbul"
+    )
+  })
 })
